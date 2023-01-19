12b238048c5feb9f35f2b3358af55ec268127a60
tashfeenzaidi <tashfeenzaidi15@gmail.com>
2023-01-19 12:56:51 +0500

    toggle added

--- iosSwiftUiExample/firstScreen.swift
+++ iosSwiftUiExample/firstScreen.swift
@@ -10,21 +10,23 @@ var tap : String = "hello world"
 struct firstScreen: View {
     var body: some View {
         GroupBox(label:
-                Label("End-User Agreement", systemImage: "building.columns")
-            ) {
-                ScrollView(.vertical, showsIndicators: true) {
-                    Text(tap)
-                        .font(.footnote)
-                }
-                .frame(height: 100)
-                
+                    Label("End-User Agreement", systemImage: "building.columns")
+        ) {
+            ScrollView(.vertical, showsIndicators: true) {
+                Text(tap)
+                    .font(.footnote)
             }
-        
+            .frame(height: 100)
+            Toggle(isOn: .constant(true)) {
+                Text("I agree to the above terms")
+            }
+            
+        }
     }
-}
-
-struct firstScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        firstScreen()
+    
+    struct firstScreen_Previews: PreviewProvider {
+        static var previews: some View {
+            firstScreen()
+        }
     }
 }
